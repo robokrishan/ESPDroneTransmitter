cb6e94f7c97d3408846806afa032b13c4ee1aa49
Harit Krishan <45740262+robokrishan@users.noreply.github.com>
2021-08-22 16:43:46 +0200

    Arduino Sketch

---
+++ ESPDroneTransmitter_test.ino
@@ -0,0 +1,26 @@
+#include <ESPDroneTransmitter.h>
+
+ESPDroneTransmitter transmitter;
+
+void setup() {
+  // put your setup code here, to run once:
+  printStep("Transmitter Initialized");
+  transmitter.initTransmitter();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  transmitter.transmit();
+  delay(50);
+}
+
+void printStep(char* msg) {
+  if(transmitter.getStep() == 0) {
+    Serial.begin(9600);
+  }
+  Serial.print("\t[");
+  Serial.print(transmitter.getStep());
+  Serial.print("]\t");
+  Serial.println(msg);
+  transmitter.incrementStep();
+}
